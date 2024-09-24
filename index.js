4c5d067601ed33add738609cfb8a853b49381931
AmnaSaeed733 <amnasaeed8800@gmail.com>
2024-09-24 07:56:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// Question Number01
+
+alert("AMNA SAEED");
+
+// Question Number02
+
+name = "Amna Saeed"
+info = "i was curently learning about Javascript in SMIT"
+alert("My name is" + name + "and" + info)
+console.log("My name is" + name + "and" + info)
+
+// Question Number03
+
+var a = 4
+var b = 2
+var result = (a++ + --b - --a + b++ + a + b)
+console.log("The result of this equation is" + result)
+// Question Number04
+
+alert("Enter your valid information so you can buy tickets for movie")
+var age = prompt("Enter your age")
+if (age <= 5) {
+    console.log("The ticket is free under 5")
+}
+if (age > 5 && age <= 15) {
+    console.log("The price of ticket for teens is RS1000")
+}
+if (age > 15 && age <= 40) {
+
+    console.log("The ticket fee for adultsis Rs2000")
+}
